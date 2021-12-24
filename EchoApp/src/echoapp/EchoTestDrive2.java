10eacd116d3a056947dd461d108d5d3cfec84b80
Angelina <angelania@ya.ru>
2021-12-24 23:55:18 +0200

    added EchoApp

---
+++ EchoApp/src/echoapp/EchoTestDrive2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package echoapp;
+
+/**
+ *
+ * @author Angelina
+ */
+public class EchoTestDrive2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    void run() {
+        Echo e1 = new Echo();
+        Echo e2 = e1;
+        int x = 0;
+        while (x<4) {
+            e1.hello();
+            e1.count = e1.count+1;
+            if(x==0) {
+                e2.count = e2.count +1;
+            }
+            if(x>0) {
+                e2.count = e2.count + e1.count;
+            }
+            x=x+1;
+        }
+        System.out.println(e2.count);
+    }
+    
+}
